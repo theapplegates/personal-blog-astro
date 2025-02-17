514ff8301a2772a43d5498a26328ff6ca32efd86
Paul Applegate <thor4@bobsyouruncle.local>
2025-02-16 22:35:05 -0500

    url

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE = {
-  website: "https://the-astro.paulapplegate.com", // replace this with your deployed domain
+  website: "https://astro-blog.paulapplegate.com", // replace this with your deployed domain
   author: "Paul Applegate",
   description:
     "Hi there!, I'm Pavan Bhaskar a developer & designer. Sharing 1 quality post per week!",
